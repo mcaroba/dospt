3f0acd847812aeacbc7d04e317988649047e1841
Miguel Caro <mcaroba@gmail.com>
2018-02-26 12:34:40 +0200

    Improved handling of supergroups file

--- src/sort_supergroups.f90
+++ src/sort_supergroups.f90
@@ -4,26 +4,63 @@ subroutine sort_supergroups(ngroups, nsupergroups, ngroups_in_supergroup, group_
 
   implicit none
 
-  integer :: ngroups, nsupergroups, iostatus, temp_int, i, j
-  integer :: group_start, group_end
-  integer, allocatable :: ngroups_in_supergroup(:), group_in_supergroup(:,:)
-  character*32 :: sg_string
+  integer :: ngroups, nsupergroups, iostatus, temp_int, i, j, k
+  integer :: group_start, group_end, nlines
+  integer, allocatable :: ngroups_in_supergroup(:), group_in_supergroup(:,:), supergroup_in_this_line(:)
+  character*1024 :: sg_string
   character*1 :: sg_char
 
+! Initial parsing of supergroups file
   open(unit=10, file="supergroups", status="old")
   nsupergroups = 0
+  nlines = 0
   do
-    read(10,*, iostat=iostatus)
-    if(iostatus /= 0)exit
-    nsupergroups = nsupergroups + 1
+    read(10, '(A)', iostat=iostatus) sg_string
+    sg_char = adjustl(sg_string)
+    if(iostatus /= 0)then
+      exit
+    else if( sg_char /= "!" .and. sg_char /= "#" .and. sg_char /= " " )then
+      nsupergroups = nsupergroups + 1
+    end if
+    nlines = nlines + 1
   end do
   rewind(10)
+  allocate( supergroup_in_this_line(1:nlines) )
+  supergroup_in_this_line = 0
+  nsupergroups = 0
+  nlines = 0
+  do
+    read(10, '(A)', iostat=iostatus) sg_string
+    sg_char = adjustl(sg_string)
+    if(iostatus /= 0)then
+      exit
+    else if( sg_char /= "!" .and. sg_char /= "#" .and. sg_char /= " " )then
+      nlines = nlines + 1
+      nsupergroups = nsupergroups + 1
+      supergroup_in_this_line(nlines) = nsupergroups
+    else
+      nlines = nlines + 1
+    end if
+  end do
+  rewind(10)
+
 
   allocate( ngroups_in_supergroup(1:nsupergroups) )
   ngroups_in_supergroup = 0
   allocate( group_in_supergroup(1:nsupergroups, 1:ngroups) )
 
-  do i=1,nsupergroups
+
+  write(*,*) nsupergroups, nlines
+
+  do k = 1, nlines
+    if( supergroup_in_this_line(k) == 0 )then
+!     This is a comment or blank line
+      read(10, *)
+      cycle
+    else
+!     This is a line with supergroup info in it
+      i = supergroup_in_this_line(k)
+    end if
     sg_string = ""
     group_start = 0
     do
